f127cc364ca383151a6e47cb2730f94d7ec20fad
Leykos <manon.luc74@gmail.com>
2016-07-01 02:56:29 +0200

    SP - version 0.0001

---
+++ Space Platform/Assets/Scripts/persoController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class persoController : MonoBehaviour {
+
+    //Déclarations des variables
+    public Animator anim;
+    public float speed = 5f;
+    public float jumpPower = 500f;
+    public Rigidbody2D perso;
+
+    public Transform checksol;
+    bool toucheLeSol = false;
+    float rayonSol = 0.3f;
+    public LayerMask Sol;
+
+    bool aDroite = true;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Récupération du component animator
+        anim = GetComponent<Animator>();
+        perso = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Récuparation de l'input de l'axe Horizontal
+        float x = Input.GetAxis("Horizontal");
+        //Donne a la variable speed (dans Unity) le déplacement du personnage
+        anim.SetFloat("speed", Mathf.Abs(x));
+        transform.Translate(x * speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && toucheLeSol == true)
+        {
+            perso.AddForce(new Vector2(0, 225));
+        }
+
+        //Avance
+        if (x > 0 && !aDroite)
+        {
+            changerDirection();
+        }
+
+        //Recule
+        if (x < 0 && aDroite)
+        {
+            changerDirection();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        toucheLeSol = Physics2D.OverlapCircle(checksol.position, rayonSol, Sol);
+        anim.SetBool("Sol", toucheLeSol);
+    }
+
+    void changerDirection()
+    {
+        aDroite = !aDroite;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
